7d7dace4f6a5d5c5c646cb9198d58eb54140bee2
helenyuhhh <helenyu301@gmail.com>
2024-09-28 00:30:37 -0400

    products array, post and get method added

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ const app = express();
 // port number on which you want to run the application
 app.use(express.json());
 const port = 3000;
+let getCounter;
+let postCounter;
 // res:request; res:responde
 // app.get(), one of the initial five methods
 // node is a self server application
 // request for /hello, so you need to add /hello at the end of hostaddress
-app.get('/hello', (req, res)=>{
-    res.json({"Key": "Hello World"})
+let datastore = { // declear an array to store the prodycts details
+    products:[]
+}
+app.get("/products", (req, res)=>{
+    res.json(datastore.products)
+}
+)
+app.post("/products", (req, res)=>{
+    const productData = req.body // initialize a variable to get data from json body
+    if(!productData || !productData.productID || !productData.productName || !productData.productPrice || !productData.productQty) {
+        return res.status(400).json({error:"Invalid Data"})
+    }
+    datastore.products.push(productData);
+    res.status(201).json(productData);
 
-}) 
-app.get('/first', (req, res)=>{
-    res.json({"Key": "Hello World"})
+})
 
-}) 
  
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`)
