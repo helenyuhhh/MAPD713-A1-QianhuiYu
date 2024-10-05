adf6b8582c3c2c28da7d94ecc4b2530a631db748
helenyuhhh <helenyu301@gmail.com>
2024-10-05 19:11:45 -0400

    counter and delete method updated

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import express from 'express';
 const app = express();
 // port number on which you want to run the application
 app.use(express.json());
-const port = 3000;
-// let getCounter;
-// let postCounter;
+const port = 5000;
+let getCounter=0;
+let postCounter=0;
 // res:request; res:responde
 // app.get(), one of the initial five methods
 // node is a self server application
@@ -15,10 +15,11 @@ let datastore = { // declear an array to store the prodycts details
     products:[]
 }
 app.get("/products", (req, res)=>{
-    //getCounter++;
+    getCounter++;
     // this method reqponse get the json file from the response 
     res.json(datastore.products);
     //console.log(`Get: ${getCounter} Post: ${postCounter}`)
+    console.log(`Get: ${getCounter} Post: ${postCounter}`)
 }
 )
 
@@ -30,6 +31,7 @@ app.get("/products/:productId", (req, res) => {
     // res.send(products.);
     // js method: array.find, with some logic function, since req.params.productId returns a string, we need to parse it
     // to Integer
+    getCounter++;
     const product = datastore.products.find(c => c.productId === parseInt(req.params.productId))
     if (!product) { // if no such product, return 404
         res.status(404).send('The product with given id is not found')
@@ -37,34 +39,36 @@ app.get("/products/:productId", (req, res) => {
     else {
         res.send(product)
     }
+    console.log(`Get: ${getCounter} Post: ${postCounter}`)
 })
-app.post("/product", (req, res)=>{
-    //postCounter++;
+app.post("/products", (req, res)=>{
+    postCounter++;
     const productData = req.body; // initialize a variable to get data from json body
+    
     if(!productData || !productData.productId || !productData.name || !productData.price || !productData.quantity) {
         return res.status(400).json({error:"Invalid Data"});
     }
-    datastore.products.push(productData);
-    res.status(201).json(productData);
-    //console.log(`Get: ${getCounter} Post: ${postCounter}`)
+    else {
+        datastore.products.push(productData);
+        res.status(201).json(productData);
+    }
+    
+    console.log(`Get: ${getCounter} Post: ${postCounter}`)
 
 })
-// parameter name can be anything, in this case, i call it productId
-/*app.delete("/products/:productId", (req,res) => { // it shpuld be products? with id? //Correct format: DELETE http://localhost/products/id
-    const proID = req.params // get the id from the params
-    if(proID === undifined) {
-        return res.status(418).json({error:"No such product!"});
-    }
-    datastore.products.pop(req.proID)
-})*/
+
 // delete by id is working
 app.delete("/products/:productId", (req,res) => {
     const product = datastore.products.find(c => c.productId === parseInt(req.params.productId))
+    // const index = datastore.products.findIndex(c => c.productId === parseInt(req.params.productId))
     if (!product) {
         res.status(404).send('The product with given id is not found')
     }
     else {
-        datastore.products.pop(product)
+        let index = datastore.products.indexOf(product)
+        // console.log (index) // test index of the item to be removed
+        // second parameter is the number of items you want to delete
+        const removed = datastore.products.splice(index, 1) 
         res.json(datastore.products);
     }
 })
